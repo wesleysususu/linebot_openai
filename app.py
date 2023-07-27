35b1f979d4736de84d0b385db0b95040797f616c
wesleysususu <junmin139430@gmail.com>
2023-07-27 15:47:54 +0800

    ok

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    message = TextSendMessage(text="你說的是不是" + msg)
+    message = TextSendMessage(text="大哥!你說的是不是" + msg)
     # GPT_answer = GPT_response(msg)
     # print(GPT_answer)
     line_bot_api.reply_message(event.reply_token, message)
