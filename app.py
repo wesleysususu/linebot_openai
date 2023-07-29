ad99c0a3be3f59d5563aeb28ddd260720e67e6e0
wesleysususu <junmin139430@gmail.com>
2023-07-29 10:23:41 +0800

    發現原因，程式碼當中有重複定意的回應函數

--- app.py
+++ app.py
@@ -59,11 +59,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    app.logger.info("Request body: " + body)
-
-    logging.info(f'msg={msg}')
-    logging.info('Q1' in msg)
-    logging.info('Q2' in msg)
+    print(msg)
     if 'Q1' in msg:
         message = TextSendMessage(text="你是乖寶寶")
     elif 'Q2' in msg:
@@ -75,27 +71,6 @@ def handle_message(event):
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    message = TextSendMessage(text=msg)
-    line_bot_api.reply_message(event.reply_token, message)
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
 
 @handler.add(MemberJoinedEvent)
 def welcome(event):
