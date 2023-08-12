8e4bf84d742b7ec09ae045fdf9bec95e066602dd
wesleysususu <junmin139430@gmail.com>
2023-08-12 14:49:57 +0800

    ok

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def handle_message(event):
         message = TextSendMessage(A8)
 
     else:
-        message = TextSendMessag('🌈親愛的朋友!➡️你可以重新輸入「0」即可透過主選菜單找到關鍵字解答你的問題喔‼️')
+        message = TextSendMessage('🌈親愛的朋友!➡️你可以重新輸入「0」即可透過主選菜單找到關鍵字解答你的問題喔‼️')
         line_bot_api.reply_message(event.reply_token, message)
     
 @handler.add(PostbackEvent)
